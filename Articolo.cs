2aebe720308f90be4502287c861aa716b05153b4
FRANCOHAILIMITIDELLEVOLUTO <121171151+FRANCO8724@users.noreply.github.com>
2023-11-16 18:13:46 +0100

    Funzione sconta

--- Articolo.cs
+++ Articolo.cs
@@ -8,5 +8,35 @@ namespace _10___Classe_articoli
 {
     internal class Articolo
     {
+        protected int codice;
+        protected string descrizione;
+        protected double prezzo;
+        protected bool card;
+        string[] Scontrino = new string[100];
+        int cont;
+
+        public Articolo(int _codice, string _descrizione, double _prezzo, bool _card, string[] Scontrino,int cont)
+        {
+            this.codice = _codice;
+            this.descrizione = _descrizione;
+            this.prezzo = _prezzo;
+            this.card = _card;
+        }
+
+        public double Sconta
+        {
+            get
+            {
+                if (card)
+                {
+                    return prezzo - (prezzo / 100) * 5;
+                }
+                else
+                {
+                    return prezzo;
+                }
+            }
+
+        }
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace _10___Classe_articoli
 {
     public partial class Form1 : Form
     {
+        public int _codice;
+        public string _descrizione;
+        public double _prezzo;
+        public bool _card = false;
+        public string[] Scontrino = new string[100];
+        public int _cont=0;
+
         public Form1()
         {
             InitializeComponent();
-        }
+        }     
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _card = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _codice = Convert.ToInt32(textBox1.Text);
+            _descrizione = textBox2.Text;
+            _prezzo = Convert.ToDouble(textBox3.Text);
+
+            textBox1.Text="";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            Articolo a = new Articolo(_codice, _descrizione, _prezzo, _card, Scontrino, _cont);
+
+            double sconto = a.Sconta;
+            listView1.Items.Add("prezzo: " +  sconto);
+        }
+
+
     }
 }
